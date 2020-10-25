a161a5f5a61b190b56f2e1b3070cecd6b6623793
Najib Ishaq <najib_ishaq@uri.edu>
2020-10-25 01:35:43 -0400

    moving things around

---
+++ src/utils.py
@@ -0,0 +1,4 @@
+import os
+
+BUILD_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'build'))
+PLOTS_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'plots'))
