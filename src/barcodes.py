d35e242a0fc0e17ea87884cebf8c0ff9a33b39e2
Najib Ishaq <najib_ishaq@uri.edu>
2020-10-28 11:39:21 -0400

    cleaned up and added cli arguments

--- src/barcodes.py
+++ src/barcodes.py
@@ -1,21 +1,22 @@
 import os
-from typing import Dict, Tuple, Set, List
+import time
+from typing import Dict, Tuple, Set, List, Optional
 
+import click
 import numpy as np
 from matplotlib import pyplot as plt
 from pyclam import Cluster, Manifold
 
-from src.toy_shapes import SHAPES
-from src.utils import PLOTS_PATH
+from toy_shapes import SHAPES
+from utils import PLOTS_PATH
 
-# Cluster, birth-radius, death-radius
+# Cluster -> (birth-radius, death-radius)
 Barcodes = Dict[Cluster, Tuple[float, float]]
 
 
-def create_barcodes(data: np.array, steps: int) -> Barcodes:
+def create_barcodes(data: np.array, *, steps: Optional[int] = 10**3, normalize: bool = False) -> Barcodes:
     manifold: Manifold = Manifold(data, 'euclidean').build()
-    step_size: float = -manifold.root.radius / steps
-    thresholds: np.array = np.arange(start=manifold.root.radius - step_size, stop=0, step=step_size)
+    thresholds: np.array = np.linspace(start=manifold.root.radius * (steps - 1) / steps, stop=0, num=steps)
     barcodes: Barcodes = dict()
     living_clusters: Barcodes = {manifold.root: (-1, manifold.root.radius)}
     for threshold in thresholds:
@@ -31,15 +32,27 @@ def create_barcodes(data: np.array, steps: int) -> Barcodes:
                 else:
                     barcodes[child] = (0, threshold)
         living_clusters.update({cluster: (-1, threshold) for cluster in new_births})
+
+    if normalize:  # normalize radii to [0, 1] range.
+        factor = manifold.root.radius
+        barcodes = {c: (b / factor, d / factor) for c, (b, d) in barcodes.items()}
+
     return barcodes
 
 
-def plot_barcodes(full_barcodes: Barcodes, filename: str):
+def plot_barcodes(full_barcodes: Barcodes, filename: str, *, merge: Optional[int] = None):
     cardinalities: List[int] = list(sorted({cluster.cardinality for cluster in full_barcodes}))
     barcodes_by_cardinality: Dict[int, Barcodes] = {cardinality: dict() for cardinality in cardinalities}
     [barcodes_by_cardinality[cluster.cardinality].update({cluster: lifetime})
      for cluster, lifetime in full_barcodes.items()]
-    max_radius: float = max((lifetime[1] for lifetime in full_barcodes.values()))
+
+    if merge is not None:
+        # Merges all barcodes for clusters with cardinality greater than 'merge'
+        high_cardinalities = [v for c, v in barcodes_by_cardinality.items() if c >= merge]
+        if len(high_cardinalities) > 0:
+            [high_cardinalities[0].update(h) for h in high_cardinalities[1:]]
+            barcodes_by_cardinality = {c: v for c, v in barcodes_by_cardinality.items() if c < merge}
+            barcodes_by_cardinality[merge] = high_cardinalities[0]
 
     fig = plt.figure(figsize=(16, 10), dpi=200)
     ax = fig.add_subplot(111)
@@ -64,10 +77,20 @@ def plot_barcodes(full_barcodes: Barcodes, filename: str):
     return
 
 
-def main():
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('num_points', type=int, default=10**3)
+@click.argument('steps', type=int, default=10**3)
+def main(num_points: int, steps: int):
     for shape in SHAPES:
-        barcodes = create_barcodes(SHAPES[shape](num_points=10**4).T, 10**4)
-        plot_barcodes(barcodes, os.path.join(PLOTS_PATH, f'barcodes-{shape}.png'))
+        start_time: float = time.time()
+        barcodes = create_barcodes(SHAPES[shape](num_points=num_points).T, steps=steps, normalize=True)
+        print(f'It took {time.time() - start_time:.2f} seconds for a {shape} with {num_points} points and {steps} resolution.')
+        plot_barcodes(barcodes, os.path.join(PLOTS_PATH, f'barcodes-{shape}.png'), merge=4)
     return
 
 
