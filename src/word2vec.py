021d1b959a326802e28271d941a132cae8778f0a
Najib Ishaq <najib_ishaq@my.uri.edu>
2021-02-07 13:54:39 -0500

    WIP
    started persistent-components network

--- src/word2vec.py
+++ src/word2vec.py
@@ -1,8 +1,9 @@
 import os
+import numpy as np
 from typing import List
 import click
 
-from gensim.models import Word2Vec
+import fasttext
 import csv
 
 FILE = 'foobar.csv'
@@ -10,37 +11,87 @@ FILE = 'foobar.csv'
 SRC = os.path.abspath(os.path.dirname(__file__))
 ROOT = os.path.abspath(os.path.join(SRC, '..'))
 
+
+CLAMP = False
+
+
 def loader(file) -> List[str]:
     """Load a data file and return it as a sentence."""
     with open(os.path.join(ROOT, f'{file}'), 'r') as fp:
         sentences = list(csv.reader(fp))
-        sentence = [''.join([str(int(bool(int(c)))) for c in w]) for w in sentences]
+        if CLAMP:
+            sentence = [','.join([str(int(bool(int(c)))) for c in w]) for w in sentences]
+        else:
+            sentence = [','.join(w) for w in sentences]
     return sentence
-    
+
+
 @click.group()
 def cli():
     pass
 
+
 @cli.command()
 @click.argument('files', nargs=-1)
-def digest(files):
+@click.option('--output', default='digested.txt')
+def digest(files, output):
+    """Digest the provided files into a corpus written to output."""
+    # TODO: Tom, please make this feel nicer
+    folder = 'digested'
+    os.makedirs(os.path.join(ROOT, folder), exist_ok=True)
+    filepath = os.path.join(ROOT, folder, output)
+    if os.path.exists(filepath):
+        os.remove(filepath)
+
     sentences = (loader(f) for f in files)
-    with open(os.path.join(ROOT, 'digested.txt'), 'w') as fp:
-        fp.write('\n'.join((' '.join([w for w in s]) for s in sentences)))
+    for i, s in enumerate(sentences):
+        print(f'sentence {i} with {len(s)} words')
+        with open(filepath, 'a') as fp:
+            fp.write(' '.join([w for w in s]))
+            fp.write('\n')
+
+
+def _digest(file, output):
+    with open(os.path.join(ROOT, output), 'w') as fp:
+        fp.write(' '.join([w for w in loader(file)]))
+        fp.write('\n')
+
+
+def _ingest(model, target):
+    with open(os.path.join(ROOT, target), 'r') as fp:
+        target_data = fp.readlines()
+    
+    for i, sentence in enumerate(target_data):
+        # noinspection PyBroadException
+        try:
+            np.save(f'{target}.{i}.npy', np.stack([model[word] for word in sentence.split(' ')]))
+        except Exception as _:
+            print('fucking fucked it')
+
+
+def _train(corpus):
+    return fasttext.train_unsupervised(corpus, model='skipgram')
+
 
 @cli.command()
 @click.argument('corpus')
-def ingest(corpus):
-    with open(corpus, 'r') as fp:
-        corpus = fp.readlines()
-    model = Word2Vec(sentences=[s.split(' ') for s in corpus])
-    breakpoint()
+@click.argument('target')
+def ingest(corpus, target):
+    """Ingest a corpus file and produce numpy memmaps."""
+    model = _train(corpus)
+    _ingest(model, target)
+
 
 @cli.command()
+@click.argument('corpus')
 @click.argument('files', nargs=-1)
-def predict(files):
-    breakpoint()
-    model = Word2Vec(sentences=[loader(f) for f in files])
+def splay(corpus, files):
+    """Splay all of the given files into individual numpy memmaps. The plural version of ingest."""
+    model = _train(corpus)
+    for file in files:
+        _digest(file, f'{file}.digested') 
+        _ingest(model, f'{file}.digested')
+
 
 if __name__ == "__main__":
     cli()
